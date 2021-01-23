321da64822a4b501e6d5013f6b2ae7016991bc14
Julia Tan <juliajtan@gmail.com>
2021-01-23 18:32:48 +0000

    Test axios saves and gets users

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,8 @@
-import { User } from './models/User';
+import axios from 'axios';
 
-const user = new User({ name: 'myname', age: '20' });
+// axios.post('http://localhost:3000/users', {
+//   name: 'myname',
+//   age: 20,
+// });
 
-user.on('change', () => {
-  console.log('change 1');
-});
-user.on('change', () => {
-  console.log('change 2');
-});
-user.on('save', () => {
-  console.log('saved');
-});
-
-user.trigger('change');
+axios.get('http://localhost:3000/users/1');
