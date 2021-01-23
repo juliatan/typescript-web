873b69d238a6706a78574dee240f2900db0b9a76
Julia Tan <juliajtan@gmail.com>
2021-01-23 19:01:25 +0000

    Create save that POST or PUT depending on whether user with ID already exists

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,12 @@
 import { User } from './models/User';
 
-const user = new User({ id: 1 });
-user.fetch();
-setTimeout(() => {
-  console.log(user), 4000;
-});
+// const user = new User({ id: 1 });
+// user.fetch();
+// setTimeout(() => {
+//   console.log(user), 4000;
+// });
+
+// user.set({ name: 'New Name', age: 99 });
+
+const user = new User({ name: 'JT', age: 10 });
+user.save();
