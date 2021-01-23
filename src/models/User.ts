c894059d9e130a0221afd0ce32935de0b4303fe8
Julia Tan <juliajtan@gmail.com>
2021-01-23 17:52:28 +0000

    Add User model that has a get and set data properties method

---
+++ src/models/User.ts
@@ -0,0 +1,23 @@
+interface UserProps {
+  name?: string;
+  age?: number;
+}
+
+type Callback = () => void;
+
+export class User {
+  events: { [key: string]: Callback[] } = {};
+
+  constructor(private data: UserProps) {}
+
+  get(propName: string): string | number {
+    return this.data[propName];
+  }
+
+  set(update: UserProps): void {
+    // override and replace this.data
+    Object.assign(this.data, update);
+  }
+
+  on(eventName: string, callback: Callback): void {}
+}
