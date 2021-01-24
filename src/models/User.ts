445ad7763b226f18f2944f466c01fef26fa900eb
Julia Tan <juliajtan@gmail.com>
2021-01-24 19:04:36 +0000

    Initialise attributes for User

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,3 +1,4 @@
+import { Attributes } from './Attributes';
 import { Eventing } from './Eventing';
 import { Sync } from './Sync';
 
@@ -12,4 +13,10 @@ const rootUrl = 'http://localhost:3000/users';
 export class User {
   public events: Eventing = new Eventing();
   public sync: Sync<UserProps> = new Sync<UserProps>(rootUrl);
+  public attributes: Attributes<UserProps>;
+
+  // need to specifically use a constructor here because we need to pass in attributes as a property
+  constructor(attrs: UserProps) {
+    this.attributes = new Attributes<UserProps>(attrs);
+  }
 }
